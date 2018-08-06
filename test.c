a2de85cf739b94106dd3c285e3205c1fef94e6fc
Gidon Ersnt <gidon.ernst@unimelb.edu.au>
2018-08-06 16:43:11 +1000

    stuff

--- test.c
+++ test.c
@@ -1,49 +1,32 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void __trace_jump();
-
 unsigned int test(unsigned int x) {
-    __trace_jump();
     if(x < 128) {
-        __trace_jump();
         if(x < 64) {
-            __trace_jump();
             if(x < 32) {
-                __trace_jump();
                 return 0;
             } else {
-                __trace_jump();
                 return 1;
             }
         } else {
-            __trace_jump();
             if(x < 64 + 32) {
-                __trace_jump();
                 return 2;
             } else {
-                __trace_jump();
                 return 3;
             }
         }
     } else {
-        __trace_jump();
         if(x < 128 + 64) {
-            __trace_jump();
             if(x < 128 + 32) {
-                __trace_jump();
                 return 4;
             } else {
-                __trace_jump();
                 return 5;
             }
         } else {
-            __trace_jump();
             if(x < 128 + 64 + 32) {
-                __trace_jump();
                 return 6;
             } else {
-                __trace_jump();
                 return 7;
             }
         }
